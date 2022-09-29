eca3e788d2df3b22371dc4450ee4ad620a98c854
Bruno Braga <braga.bruno81@gmail.com>
2022-09-29 08:16:07 -0500

    cleaning up

--- app/src/main/java/com/example/acronyms/data/model/AcronymTestMoshiItem.kt
+++ app/src/main/java/com/example/acronyms/data/model/AcronymTestMoshiItem.kt
@@ -1,16 +1,4 @@
 package com.example.acronyms.data.model
 
-// @JsonClass(generateAdapter = true)
-// data class AcronymTestMoshiItem(
-//    @Json(name = "lfs")
-//    val lfs: List<LfX>,
-//    @Json(name = "sf")
-//    val sf: String
-// )
-//data class AcronymResponse(val item: AcronymTestMoshiItem)
-//data class AcronymTestMoshiItem(val a: List<AcronymList>)
 data class AcronymList(val sf: String, val lfs: List<AcronymTestItem>)
 data class AcronymTestItem(val lf: String)
-// , val lfs: List<AcronymItem>)
-
-// data class AcronymItem(val lf: String)
